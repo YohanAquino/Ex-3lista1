2bae47996a95fb499c1f53fc52c53dacd6d87ce7
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:32:59 -0300

    Add files via upload

---
+++ EX-3-LISTA-1-Yohan_Aquino/EX-3-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_3_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será digitado.
+            Quadrado qua = new Quadrado();
+
+            Console.WriteLine("digite a diagonal do quadrado:");
+            qua.setDiagonal(double.Parse(Console.ReadLine()));
+
+            qua.calcularArea();
+            Console.WriteLine("A área do quadrado é {0}", qua.getArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EX-3-LISTA-1-Yohan_Aquino/EX-3-LISTA-1-Yohan_Aquino/Quadrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_3_LISTA_1_Yohan_Aquino
+{
+    class Quadrado
+    {
+        private double d;
+        private double a;
+
+        public void setDiagonal(double x)
+        {
+            d = x;
+        }
+
+        public void calcularArea()
+        {
+            a = Math.Pow(d, 2)/2;
+        }
+        public double getArea()
+        {
+            return a;
+        }
+
+    }
+}
